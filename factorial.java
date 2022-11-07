ad316674b74309c3a55b95e7dc1e83220c530a50
thedudziak <49055317+thedudziak@users.noreply.github.com>
2022-11-07 11:02:55 -0600

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -7,11 +7,11 @@ public class factorial {
 
 
 
-		static float number ;
+		static int number ;
 		int i;
 		static int fact=1;
 		
-		static float factorial;
+		static int factorial;
 		static Scanner sc= new Scanner(System.in);
 
 	public static void main(String[] args) {
